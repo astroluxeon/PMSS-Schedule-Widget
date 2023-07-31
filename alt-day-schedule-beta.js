b548c37d2454717d5daa402c9750ad9b10715986
zichenc7 <zichen.zc.cai@gmail.com>
2023-07-31 10:14:06 -0700

    v1.1.11-beta

--- alt-day-schedule-beta.js
+++ alt-day-schedule-beta.js
@@ -1,12 +1,12 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: magic;
-// PMSS Schedule Widget v1.1.10-beta
+// PMSS Schedule Widget v1.1.11-beta
 
 const testing = true;
 
 const scriptURL = testing ? "https://raw.githubusercontent.com/zichenc7/PMSS-Schedule-Widget/master/alt-day-schedule-beta.js" : "https://raw.githubusercontent.com/zichenc7/PMSS-Schedule-Widget/master/alt-day-schedule.js";
-const version = testing ? "1.1.10" : "0.0.0";
+const version = testing ? "1.1.11" : "0.0.0";
 
 const widget = new ListWidget();
 
